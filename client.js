93f73fc59e145ce200a68669dbf2213c86eff08e
Nicsena <Nicholassena8190@gmail.com>
2020-12-14 19:33:14 -0800

    Create client.js

---
+++ client.js
@@ -0,0 +1,2 @@
+// Found on Glitch Project: discord-bot-chat. Putting here so it can be instantly reset by discord. The discord bot is in no servers.
+client.login("Mzg3NzA0NjYzNTc2ODA1Mzc3.DQyI7A.BCn14rHYD7Oj-qeXAHFjgVQ4pDk")
