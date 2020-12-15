a9e8a64aa7f981a6292e38e6757dc854abd69e44
Nicsena <Nicholassena8190@gmail.com>
2020-12-14 19:38:47 -0800

    Update client.js

--- client.js
+++ client.js
@@ -1,2 +1,4 @@
 // Found on Glitch Project: discord-bot-chat. Putting here so it can be instantly reset by discord. The discord bot is in no servers.
 client.login("Mzg3NzA0NjYzNTc2ODA1Mzc3.DQyI7A.BCn14rHYD7Oj-qeXAHFjgVQ4pDk")
+// Found on Glitch Prject: discordbot12333. The discord bot is in no servers. To prevent your bot getting hacked, put the token in .env and put client.login(process.env.TOKEN) in the js file.
+client.login("NTU2ODA3OTYwNzQ4MjI4NjQx.D2_HqQ.e_vDh9M-E4chuYLiBjGxWO09Mxs")
