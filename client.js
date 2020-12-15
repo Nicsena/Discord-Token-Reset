41ee9fa583fc86d56fe087f55f81e7405990f10a
Nicsena <Nicholassena8190@gmail.com>
2020-12-14 20:40:11 -0800

    Update client.js

--- client.js
+++ client.js
@@ -6,3 +6,13 @@ client.login("NTU2ODA3OTYwNzQ4MjI4NjQx.D2_HqQ.e_vDh9M-E4chuYLiBjGxWO09Mxs")
 client.login("NTU2ODI0MDkyODk2OTE5NTU2.D2_W9Q.kF_vYerc_30fqW1y3r7cMeRm6_E")
 // Found on Glitch Project: discord-bot-creator. The discord bot is in no servers.
 client.login("NjE0MjY3MDgwMzUwNjI5OTAx.XWqV_A.Md59OZoIfJOxUvq3jlRHqXRnZyE")
+
+//------------------------
+
+// Found on a Glitch Project. Looks like the server that the bot is in got destroyed. The bot is in one server.
+client.login("Nzc5NDg4MjE5NDE3OTM1OTEy.X7hQ-Q.GGNSVfpK482oAGA3xPRH1am2rLo")
+             
+             
+             
+             
+// If you want to talk to me on discord, you can, add me: Nicholas#6825 (ID: 270700098621669378)
