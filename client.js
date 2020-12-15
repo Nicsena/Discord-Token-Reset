b5dd9a33c6abe98e71e3fe225de707d23e08783a
Nicsena <Nicholassena8190@gmail.com>
2020-12-14 19:49:09 -0800

    Update client.js

--- client.js
+++ client.js
@@ -2,3 +2,5 @@
 client.login("Mzg3NzA0NjYzNTc2ODA1Mzc3.DQyI7A.BCn14rHYD7Oj-qeXAHFjgVQ4pDk")
 // Found on Glitch Prject: discordbot12333. The discord bot is in no servers. To prevent your bot getting hacked, put the token in .env and put client.login(process.env.TOKEN) in the js file.
 client.login("NTU2ODA3OTYwNzQ4MjI4NjQx.D2_HqQ.e_vDh9M-E4chuYLiBjGxWO09Mxs")
+// Found on Glitch Project: juniper-target. The discord bot is in no servers.
+client.login("NTU2ODI0MDkyODk2OTE5NTU2.D2_W9Q.kF_vYerc_30fqW1y3r7cMeRm6_E")
