58e26b786c8656246567516957a09dab6e938899
Nicsena <Nicholassena8190@gmail.com>
2020-12-14 20:48:44 -0800

    Update client.js

--- client.js
+++ client.js
@@ -1,18 +1,21 @@
 // Found on Glitch Project: discord-bot-chat. Putting here so it can be instantly reset by discord. The discord bot is in no servers.
 client.login("Mzg3NzA0NjYzNTc2ODA1Mzc3.DQyI7A.BCn14rHYD7Oj-qeXAHFjgVQ4pDk")
+
 // Found on Glitch Prject: discordbot12333. The discord bot is in no servers. To prevent your bot getting hacked, put the token in .env and put client.login(process.env.TOKEN) in the js file.
 client.login("NTU2ODA3OTYwNzQ4MjI4NjQx.D2_HqQ.e_vDh9M-E4chuYLiBjGxWO09Mxs")
+
 // Found on Glitch Project: juniper-target. The discord bot is in no servers.
 client.login("NTU2ODI0MDkyODk2OTE5NTU2.D2_W9Q.kF_vYerc_30fqW1y3r7cMeRm6_E")
+
 // Found on Glitch Project: discord-bot-creator. The discord bot is in no servers.
 client.login("NjE0MjY3MDgwMzUwNjI5OTAx.XWqV_A.Md59OZoIfJOxUvq3jlRHqXRnZyE")
 
 //------------------------
 
-// Found on a Glitch Project. Looks like the server that the bot is in got destroyed. The bot is in one server.
+// Found on Glitch Project: discord-music-bot-code. Looks like the server that the bot is in got destroyed. The bot is in one server.
 client.login("Nzc5NDg4MjE5NDE3OTM1OTEy.X7hQ-Q.GGNSVfpK482oAGA3xPRH1am2rLo")
 
-// Found on a Glitch Project. looks like the same server that the discord bot above is in. The bot is in the same server as the above one.
+// Found on two Glitch Projects: psychedelic-remarkable-lightning and common-spectacular-quail. looks like the same server that the discord bot above is in. The bot is in the same server as the above one.
 client.login("NzgwNDkwNTc1NjM5MjE2MTM4.X7v2fg.mWsyCuq9GZ3k22p2Ty8d4eS3S9Y")
              
              
