e9bad7ea0745f421217045572a535e754a1694ee
Nicsena <Nicholassena8190@gmail.com>
2020-12-15 20:34:06 -0800

    Added 15+ Discord Bot Tokens so it can be reset.

--- client.js
+++ client.js
@@ -67,5 +67,127 @@ client.login("NzA1MTMwMzY5NjY1NDY2NDAy.XqnOMg.IDaeltbWSWXfKmxM94dvMQPUq2k")
 // Found on one of my friend's glitch projects. The bot is in 1 server.
 client.login("NzYyODg4MjEzOTU3OTY3ODgz.X3vtAw.l4_NLn_5g3n-4ixLWh2J4nUXRx4")
 
+// -----DEC 15, 2020-----
+
+// NOTE: I'M DOING THIS TO MAKE WORKING DISCORD TOKENS TO BE RESET BY DISCORD SAFETY JIM TO MAKE SURE THAT OTHER DEVELOPERS' BOTS
+// NOT TO GET HACKED AND DESTROY SERVERS. PLEASE READ THE MINI TUTORIAL BELOW!
+
+// PLEASE PUT THE BOT TOKEN IN .env TO PREVENT GETTING YOUR BOT HACKED BY OTHER PEOPLE!
+
+// Mini Tutorial:
+// 1) Make a file named .env
+
+// 2) Go into the file named: .env and put TOKEN=<YOUR-DISCORD-BOT-TOKEN>
+// 2A) Replace <YOUR-DISCORD-BOT-TOKEN> with your Discord Bot Token
+
+// 3) Go to the main bot js file and put client.login(process.env.TOKEN) or bot.login(process.env.TOKEN)
+
+// 4) Remove the code where it contains your bot's token or replace the code with your bot's token with process.env.TOKEN
+
+// 5) Restart the Project
+
+// 6) There you go. Your bot's token can't be found by other people since it's in .env
+
+
+// Found on the Glitch Project: darkland-bot. The bot is in no servers.
+client.login("Nzg3NTQ5MzU4ODE1MDUxNzg2.X9Wkfw.63xoqL6J_ynHmNRXTchqqMYOeh8")
+
+// Found on the Glitch Project: plant-kindly-brook and incandescent-valley-source. The bot is in no servers.
+client.login("Nzg3MjEwMDMxNDAxNTMzNDcw.X9RoeQ.5DssjaziMYVXPPPf5sMzZuD1U6w")
+
+// Found on the Glitch Project: astro-musics. The bot is in no servers.
+client.login("Nzg4MDYxMDkzNTU2NzgxMDg2.X9eBFg.vS69uAfGF9GP8hZqcVzvF_w1ncE")
+
+// Found on the Glitch Project: notch-omniscient-English. The bot is in no servers.
+client.login("Nzg3NTI0OTA1MzEzNzYzMzQ4.X9WNuA.PVApF0yuaxgDI4JA3ZDkzYzWq9k")
+
+// Found on the Glitch Project: j4j-giveaways. The bot is in no servers. client.login("Nzg2MjYzNDgxNjUyODcxMTg4.X9D27g.LKv7y-HlfWk1ExUJzv0AFbZOcnI")
+
+// Found on the Glitch Project: darkened-flash-flame and sordid-bristle-English and goldenrod-vine-talk and sky-shore-server. The bot is in no servers.
+client.login("Nzc2MTkxMDM1Nzc5OTczMTYw.X6xSOw.dDqed20uE7iNtNVKev-1Olp7K8k")
+
+// ---- Discord Gen Bots. Why? -----
+
+// Found on the Glitch Project: resisted-awesome-lipstick. The bot is in no servers.
+client.login("NzgwMDQ0MjUxMTUyMzg0MDEx.X7pW0Q.kbV23spLEPG9YNw8TMlghD1s1mE")
+
+// Found on the Glitch Project: sturdy-pitch-paprika. The bot is in no servers.
+client.login("Nzg2MjY5NzM5MzEwNzc2MzUw.X9D8wg.OvX63-gkmkJale_4Qq_13_l69Gs")
+
+// Found on the Glitch Project: grape-concise-harmonica. The bot is no servers.
+client.login("Nzg2MjYzMzk0ODcyOTgzNTUy.X9D22Q.eeOk5rmFjtNZs8i3O1gxXWEUG58")
+
+// --------------------------------
+
+
+// Found on the Glitch Project: android-music-bot. The bot is in no servers.
+client.login("NzgzMjYxMDAwNjgxMjU5MDA4.X8YKpg.Q07oYXLLGRs3pIolUDR418mb-tg")
+
+// Found on the Gltich Project: rewardaanobot. The bot is in no servers.
+// Discord Reward Bot.
+client.login("Nzc5Nzc5NTc2MjI1NDY0MzIw.X7lgUg.sNShZLme_KO69us3UdkAS1uzNsY")
+
+// Found on the Glitch Project: securitybotano. The bot is in no servers.
+client.login("Nzc5NjgzNzU3MDQ0NzkzMzU1.X7kHFQ.w9SY13F-dkNKHrwt-zRRp8Wuyrs")
+
+// Found on the Glitch Project: certain-likeable-ceratonykus. The bot is in no servers.
+client.login("Nzc4NjY0OTQxNzg3Njc2Nzcz.X7VSPQ.Wqh4LeaHaT8eb6A2rchrQYsCkxw")
+
+// Found on the Glitch Project: silly-mire-deal. the bot is in no servers.
+client.login("NzczNjM0NTA1MzQzNzYyNDUy.X6MFRw.hEUp8Og_-bCk868QiuUHXEZUTns")
+
+// Found on the Glitch Project: fern-pool-tellurium. The bot is in no servers.
+client.login("Nzg4MjYwOTg0NjY1NDA3NTA4.X9g7Pw.pnPXWEdSIB29cfp6DL8kG4OR5cc")
+
+// Found on the Glitch Project: tranquil-vintage-sailfish. The bot is in no servers.
+client.login("Nzg4MDQ4MTA0NzYxOTgyOTk3.X9d0_Q.5dPItpTOuFsZ6_YTeBSg2ZxrOAo")
+
+// Found on the Glitch Project: bot-musicdiscord. The bot is in no servers.
+client.login("NzgzOTMwNTI0NDMwNjMwOTE0.X8h6MQ.rJ2qSSNZLOZC1lpVR0EU-QY4oZs")
+
+// Found on Glitch Project: marked-amethyst-blender. The bot is in 10+ servers.
+client.login("NzgyMTYxMTI3MDYwNjY4NDI3.X8IKUA.lChNoINAM0oo8Q-PdBADGEPO2Xo")
+
+// Found on Glitch Project: daffy-maize-tuna. The bot is in 5+ servers.
+client.login("NzgwNzY2NTk3MTMxNjY1NDA4.X7z3jg.Ia0ac-we10QegZ3PveFNkKNS2hI")
+
+// Found on Glitch Project: gleaming-wry-team. The bot is in 2 servers.
+client.login("NzE4Njc4ODg2NjM1OTI5NjQx.XtsX3g.n4i4v4co5GUBE-Vsja4EKHG__hQ")
+
+// Found on Glitch Project: rhetorical-secret-wool. The bot is in 2 servers.
+client.login("Nzg0NTIzNDc1NDIzMTk5MjMy.X8qibA.USuqjssvIPT7cDxdIfTFVjR1p-0")
+
+// Found on Glitch Project: pretty-paper-hellebore. The bot is in 1 server.
+client.login("Nzg4NDgxODY2NTI4NzE4ODc5.X9kI9g.ytqxiFk951bn3UUj1mt1_ALP97o")
+
+// Found on Glitch Project: wary-humane-king. The bot is in 1 server.
+client.login("Nzg4NDgzMjAyMzEwMjA5NjE3.X9kKNA.hEE3JPDnJg44gwBHcgfa7Zfl2fM")
+
+// Found on Glitch Project: proctection. The bot is in 1 server.
+client.login("Nzg4NDIyNjIxMTgzNjA2Nzg2.X9jRyQ.voLLfgZK_gsCkT5JdgPSa-mti7Y")
+
+// Found on Glitch Project: ringed-brief-measure. The bot is in 1 server.
+client.login("NzcyNjk3MjMwOTM4ODY1NjY1.X5-cXw.HjQwDRyEFhexP7J4XVIx4QPfBLI")
+
+// Found on Glitch Project: ash-foil-vertebra. The bot is in 1 server.
+client.login("Nzg3NTc2NTY0ODc5MzI3Mjgz.X9W91Q.csOwEvlL_LJ1HgHvrIQ4ZZVC2c4")
+
+// Found on Glitch Project: large-fifth-reptile. The bot is in 1 server.
+client.login("Nzg4NDMwMzY5NjQwMDIyMDY3.X9jZAA.VqKe0tHTSuNC1NOrIntV4UwyDro")
+
+// Found on Glitch Project: modern-island-mortarboard. The bot is in 3 Servers.
+client.join("NzgxNzA0MjA1NTk2NjIyODg5.X8BgxQ.346r_MYuSxHCXFvahBz72I8tmyA")
+
+// Found on Glitch Project: delicate-tall-room. The bot is in 4 servers.
+client.login("NzgxNDgxNjI0MjA2NDQyNTE2.X7-Reg.Otem_Pv-AssZ93tEBxSfPJXL6_Y")
+
+// Found on Glitch Project: music-bot-baxterhatdogmusic-bot-baxter. The bot is in 0 servers.
+client.login("Nzg3MTI3MDAxMTczMzI3ODcy.X9QbJQ.q33iKeWHoSz3zY1VjnIZpL8T04k")
+
+// Found on Glitch Project: crafter-. The bot is in 1 server.
+client.login("Nzg1NzUxODUxODM5NzgyOTIy.X88acA.YzaEgXnQydkYUokf7CXCI1erV6E")
+
+// LOOK UP ABOVE FOR A MINI TUTORIAL!
+
 // NOTE: PLEASE PUT YOUR DISCORD BOT TOKENS IN THE .ENV FILE! THIS IS BAD THAT I FOUND A LOT OF DISCORD BOT TOKENS!
-// -- PUT YOUR TOKEN IN .env AND PUT client.login(process.env.TOKEN0 in the bot js file!
+// -- PUT YOUR TOKEN IN .env AND PUT client.login(process.env.TOKEN) in the bot js file!
